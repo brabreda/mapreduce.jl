74493d8f2f97c0561b57d2704eb07314ecebb1fd
brabreda <bram.bfh@gmail.com>
2024-01-07 19:31:56 +0100

    NVTX profiling

--- src/CUDAlaunch.jl
+++ src/CUDAlaunch.jl
@@ -1,10 +1,12 @@
 # launch configuration
-function launch_config(kernelObj, args...; workgroupsize, ndrange)
+NVTX.@annotate function launch_config(kernelObj, args...; workgroupsize, ndrange)
   backend = KernelAbstractions.backend(kernelObj)
   ndrange, workgroupsize, iterspace, dynamic = KernelAbstractions.launch_config(kernelObj, ndrange ,workgroupsize)
   ctx = KernelAbstractions.mkcontext(kernelObj, ndrange, iterspace)
-
+  
+  NVTX.@range "@cuda launch=false" begin
   kernel = CUDA.@cuda launch=false always_inline=backend.always_inline kernelObj.f(ctx, args...)
+  end
 
   maxblocks, maxthreads = CUDA.launch_configuration(kernel.fun)
 
