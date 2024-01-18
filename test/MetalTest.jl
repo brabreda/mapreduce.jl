d3abc1419169bf8c535817d76603b1feb3411944
brabreda <bram.bfh@gmail.com>
2024-01-18 21:15:59 +0100

    Metal does not support F64

--- test/MetalTest.jl
+++ test/MetalTest.jl
@@ -6,7 +6,7 @@ include("../src/Metallaunch.jl")
 include("base.jl")
 
 eltypes = ( Int16, Int32, Int64,
-            Float16, Float32, Float64,
+            Float16, Float32,
             ComplexF16, ComplexF32,
             Complex{Int16}, Complex{Int32}, 
             Complex{Int64})
